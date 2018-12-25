9ea1bb50d4da527b6a785f793b7edcc77fdda077
huangyuzai <1792132332@qq.com>
2018-12-25 23:32:16 +0800

    完成弹出框，完成商品页左边菜单

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,15 +1,68 @@
 <template>
   <div class="Goods">
-    我是商品区
+    <!-- 左边菜单 -->
+    <div class="menu-wrapper">
+      <ul class="menu-nav">
+        <li class="menu-item" v-for="item of goods" :key="item.index">
+          {{item.name}}
+        </li>
+      </ul>
+    </div>
+    <!-- 右边商品区 -->
+    <div class="goods-wrapper"></div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+const ERR_OK = 0 // 定义错误码
 export default {
-  name: 'Goods'
+  name: 'Goods',
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      goods: [] // 商品数据
+    }
+  },
+  mounted () {
+    // 请求商品数据
+    axios.get('/api/goods').then((res) => {
+      const data = res.data
+      if (data.errno === ERR_OK) {
+        this.goods = data.data
+      }
+    })
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.Goods
+  display: flex
+  position: absolute
+  top: 174px
+  bottom: 50px
+  width: 100%
+  .menu-wrapper
+    width: 80px
+    flex: 0 0 80px
+    background: #f3f5f7
+    box-sizing: border-box
+    .menu-nav
+      .menu-item
+        font-size: 12px
+        color: rgb(7,17,27)
+        font-weight: 200
+        line-height: 14px
+        height: 54px
+        display: flex
+        align-items: center
+        border-bottom: solid 1px rgba(7,17,27,.1)
+        padding: 0 12px
+  .goods-wrapper
+    flex: 1
 </style>
--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -33,39 +33,40 @@
       <img :src="seller.avatar" alt="" width="100%" >
     </div>
     <!-- 商品详情弹出框 -->
-    <div class="sellerDetail" v-show="showDetail">
-      <div class="detailContainer clearfix">
-        <div class="detailMain">
-          <h1 class="detailTitle">{{seller.name}}</h1>
-          <div class="starWrapper">
-            <star :size="48" :score="seller.score"></star>
+    <transition name="fade">
+      <div class="sellerDetail" v-show="showDetail">
+        <div class="detailContainer clearfix">
+          <div class="detailMain">
+            <h1 class="detailTitle">{{seller.name}}</h1>
+            <div class="starWrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="discountTitle">
+              <span></span>
+              <h2>优惠信息</h2>
+              <span></span>
+            </div>
+            <div class="discountInfo">
+              <ul>
+                <li v-for="item of seller.supports" :key='item.index' v-if="seller.supports">
+                  <span class="discountIcon" :class="iconType[item.type]"></span>
+                  <span class="discountDesc">{{item.description}}</span>
+                </li>
+              </ul>
+            </div>
+            <div class="discountTitle">
+              <span></span>
+              <h2>商家公告</h2>
+              <span></span>
+            </div>
+            <p class="announcement">{{seller.bulletin}}</p>
           </div>
-          <div class="discountTitle">
-            <span></span>
-            <h2>优惠信息</h2>
-            <span></span>
-          </div>
-          <div class="discountInfo">
-            <ul>
-              <li v-for="item of seller.supports" :key='item.index' v-if="seller.supports">
-                <span class="discountIcon" :class="iconType[item.type]"></span>
-                <span class="discountDesc">{{item.description}}</span>
-              </li>
-            </ul>
-          </div>
-          <div class="discountTitle">
-            <span></span>
-            <h2>商家公告</h2>
-            <span></span>
-          </div>
-          <p class="announcement">{{seller.bulletin}}</p>
+        </div>
+        <div class="detailClose" @click="hideDetailBtn">
+          <i class="icon-close"></i>
         </div>
       </div>
-      <div class="detailClose" @click="hideDetailBtn">
-        <i class="icon-close"></i>
-      </div>
-      <div class="detailBg"></div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -103,225 +104,233 @@ export default {
 <style lang="stylus" scoped>
 @import '../../common/stylus/mlxin.styl'
 @import '../../common/stylus/main.styl'
-  .AppHeader
-    background-size: 100% !important
-    position: relative
+.fade-enter,
+.fade-leave-to
+  opacity: 0
+
+.fade-enter-active,
+.fade-leave-active
+  transition: opacity 0.2s
+
+.AppHeader
+  background-size: 100% !important
+  position: relative
+  overflow: hidden
+  background-color: rgba(7,17,27,.5)
+  .headerContent
+    padding: 0 12px
     overflow: hidden
-    background-color: rgba(7,17,27,.5)
-    .headerContent
-      padding: 0 12px
+    padding-bottom: 18px
+    padding-top: 24px
+    position: relative
+    .avatar
+      width: 64px
+      height: 64px
+      border-radius: 2px
       overflow: hidden
-      padding-bottom: 18px
-      padding-top: 24px
-      position: relative
-      .avatar
-        width: 64px
-        height: 64px
-        border-radius: 2px
+      display: inline-block
+      float: left
+      margin: 0px 18px 0 12px
+      img
+        width: 100%
+        height: 100%
+    .storeInfo
+      float: left
+      padding: 2px 0
+      .storeTitle
+        margin-bottom: 8px
         overflow: hidden
-        display: inline-block
-        float: left
-        margin: 0px 18px 0 12px
-        img
-          width: 100%
-          height: 100%
-      .storeInfo
-        float: left
-        padding: 2px 0
-        .storeTitle
-          margin-bottom: 8px
-          overflow: hidden
-          .storeIcon
-            width: 30px
-            height: 18px
-            float: left
-            margin-right: 6px
-            background-color: #ddd
-            bg-image('../../../static/image/brand')
-            background-size: 30px 18px
-          h1
-            font-size: 16px
-            color: rgb(255,255,255)
-            line-height: 18px
-            font-weight: bold
-            float: left
-        .Distribution
-          font-size: 12px
+        .storeIcon
+          width: 30px
+          height: 18px
+          float: left
+          margin-right: 6px
+          background-color: #ddd
+          bg-image('../../../static/image/brand')
+          background-size: 30px 18px
+        h1
+          font-size: 16px
           color: rgb(255,255,255)
-          font-weight: 200
-          line-height: 12px
-          margin-bottom: 10px
-        & .supports
-          font-size: 10px
-          color: rgb(255,255,255)
-          font-weight: 200
-          line-height: 12px
-        & .offer
-          overflow: hidden
-          .offIcon
-            float: left
-            margin-right: 4px
-            width: 12px
-            height: 12px
-            background-color: #ddd
-            background-size: 12px
-            background-repeat: no-repeat
-            &.decrease
-              bg-image('../../../static/image/decrease_1')
-            &.discount
-              bg-image('../../../static/image/discount_1')
-            &.special
-              bg-image('../../../static/image/special_1')
-            &.invoice
-              bg-image('../../../static/image/invoice_1')
-            &.guarantee
-              bg-image('../../../static/image/guarantee_1')
-          .supports
-            float: left
-            line-height: 12px
-      .descNum
-        position: absolute
-        bottom: 15px
-        right: 12px
-        background: rgba(0,0,0,.2)
-        border-radius: 15px
+          line-height: 18px
+          font-weight: bold
+          float: left
+      .Distribution
+        font-size: 12px
         color: rgb(255,255,255)
-        font-size: 10px
         font-weight: 200
         line-height: 12px
-        height: 24px
-        box-sizing: border-box
-        padding: 7px 8px
-        vertical-align: top
-    .bulletin
-      height: 28px
-      padding: 0 12px
-      overflow: hidden
-      background: rgba(7,17,27,.2)
-      line-height: 28px
-      font-size: 0
-      display: flex
-      align-items: center
-      .bulletinIcon
-        bg-image('../../../static/image/bulletin')
-        background-size: 22px 12px
-        background-repeat: no-repeat
-        display: inline-block
-        width: 22px
-        height: 12px
-        margin-right: 4px
-      .bulletinDesc
-        display: inline-block
+        margin-bottom: 10px
+      & .supports
         font-size: 10px
         color: rgb(255,255,255)
         font-weight: 200
-        width: 315px
-        margin-right: 4px
-        ellipsis()
-    .backgroundImg
+        line-height: 12px
+      & .offer
+        overflow: hidden
+        .offIcon
+          float: left
+          margin-right: 4px
+          width: 12px
+          height: 12px
+          background-color: #ddd
+          background-size: 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('../../../static/image/decrease_1')
+          &.discount
+            bg-image('../../../static/image/discount_1')
+          &.special
+            bg-image('../../../static/image/special_1')
+          &.invoice
+            bg-image('../../../static/image/invoice_1')
+          &.guarantee
+            bg-image('../../../static/image/guarantee_1')
+        .supports
+          float: left
+          line-height: 12px
+    .descNum
+      position: absolute
+      bottom: 15px
+      right: 12px
+      background: rgba(0,0,0,.2)
+      border-radius: 15px
+      color: rgb(255,255,255)
+      font-size: 10px
+      font-weight: 200
+      line-height: 12px
+      height: 24px
+      box-sizing: border-box
+      padding: 7px 8px
+      vertical-align: top
+  .bulletin
+    height: 28px
+    padding: 0 12px
+    overflow: hidden
+    background: rgba(7,17,27,.2)
+    line-height: 28px
+    font-size: 0
+    display: flex
+    align-items: center
+    .bulletinIcon
+      bg-image('../../../static/image/bulletin')
+      background-size: 22px 12px
+      background-repeat: no-repeat
+      display: inline-block
+      width: 22px
+      height: 12px
+      margin-right: 4px
+    .bulletinDesc
+      display: inline-block
+      font-size: 10px
+      color: rgb(255,255,255)
+      font-weight: 200
+      width: 315px
+      margin-right: 4px
+      ellipsis()
+  .backgroundImg
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+    z-index: -1
+    filter: blur(10px)
+  .sellerDetail
+    width: 100%
+    height: 100%
+    background: rgba(7,17,27,.8)
+    position: fixed
+    top: 0
+    left: 0
+    z-index: 9
+    overflow: auto
+    .detailContainer
+      width: 100%
+      min-height: 100%
+      .detailMain
+        margin-top: 64px
+        padding-bottom: 64px
+        .detailTitle
+          font-size: 16px
+          font-weight: 700
+          color: rgb(255,255,255)
+          line-height: 16px
+          text-align: center
+          margin-bottom: 16px
+        .starWrapper
+          text-align: center
+          padding: 2px 0
+          margin-bottom: 28px
+        .discountTitle
+          font-size: 16px
+          color: #ffffff
+          text-align: center
+          font-weight: 700
+          padding: 0 36px
+          display: flex
+          align-items: center
+          justify-content: center
+          margin-bottom: 24px
+          h2
+            padding: 0 12px
+          span
+            display: block
+            height: 2px
+            flex: 1
+            background: rgba(255,255,255,.2)
+        .discountInfo
+          padding: 0 36px
+          margin-bottom: 28px
+          ul
+            padding: 0 12px
+            color: rgb(255,255,255)
+            li
+              overflow: hidden
+              margin-bottom: 12px
+              line-height: 12px
+              .discountIcon
+                display: inline-block
+                height: 16px
+                width: 16px
+                margin-right: 6px
+                float: left
+                background-color: #ddd
+                background-size: 16px
+                background-repeat: no-repeat
+              .discountDesc
+                font-size: 12px
+                font-weight: 200
+              .decrease
+                bg-image('../../../static/image/decrease_1')
+              .discount
+                bg-image('../../../static/image/discount_1')
+              .special
+                bg-image('../../../static/image/special_1')
+              .invoice
+                bg-image('../../../static/image/invoice_1')
+              .guarantee
+                bg-image('../../../static/image/guarantee_1')
+        .announcement
+          padding: 0 48px
+          font-size: 12px
+          font-weight: 200
+          color: #fff
+          line-height: 24px
+    .detailClose
+      position: relative
+      width: 32px
+      height: 32px
+      margin: -64px auto 0 auto
+      clear: both
+      font-size: 32px
+      color: rgba(255,255,255,.5)
+    .detailBg
+      width: 100%
+      height: 100%
       position: absolute
       top: 0
       left: 0
-      width: 100%
-      height: 100%
-      z-index: -1
-      filter: blur(10px)
-    .sellerDetail
-      width: 100%
-      height: 100%
       background: rgba(7,17,27,.8)
-      position: fixed
-      top: 0
-      left: 0
-      z-index: 9
-      overflow: auto
-      .detailContainer
-        width: 100%
-        min-height: 100%
-        .detailMain
-          margin-top: 64px
-          padding-bottom: 64px
-          .detailTitle
-            font-size: 16px
-            font-weight: 700
-            color: rgb(255,255,255)
-            line-height: 16px
-            text-align: center
-            margin-bottom: 16px
-          .starWrapper
-            text-align: center
-            padding: 2px 0
-            margin-bottom: 28px
-          .discountTitle
-            font-size: 16px
-            color: #ffffff
-            text-align: center
-            font-weight: 700
-            padding: 0 36px
-            display: flex
-            align-items: center
-            justify-content: center
-            margin-bottom: 24px
-            h2
-              padding: 0 12px
-            span
-              display: block
-              height: 2px
-              flex: 1
-              background: rgba(255,255,255,.2)
-          .discountInfo
-            padding: 0 36px
-            margin-bottom: 28px
-            ul
-              padding: 0 12px
-              color: rgb(255,255,255)
-              li
-                overflow: hidden
-                margin-bottom: 12px
-                line-height: 12px
-                .discountIcon
-                  display: inline-block
-                  height: 16px
-                  width: 16px
-                  margin-right: 6px
-                  float: left
-                  background-color: #ddd
-                  background-size: 16px
-                  background-repeat: no-repeat
-                .discountDesc
-                  font-size: 12px
-                  font-weight: 200
-                .decrease
-                  bg-image('../../../static/image/decrease_1')
-                .discount
-                  bg-image('../../../static/image/discount_1')
-                .special
-                  bg-image('../../../static/image/special_1')
-                .invoice
-                  bg-image('../../../static/image/invoice_1')
-                .guarantee
-                  bg-image('../../../static/image/guarantee_1')
-          .announcement
-            padding: 0 48px
-            font-size: 12px
-            font-weight: 200
-            color: #fff
-            line-height: 24px
-      .detailClose
-        position: relative
-        width: 32px
-        height: 32px
-        margin: -64px auto 0 auto
-        clear: both
-        font-size: 32px
-        color: rgba(255,255,255,.5)
-      .detailBg
-        width: 100%
-        height: 100%
-        position: absolute
-        top: 0
-        left: 0
-        background: rgba(7,17,27,.8)
-        filter: blur(10px)
-        z-index: -1
+      filter: blur(10px)
+      z-index: -1
 </style>
