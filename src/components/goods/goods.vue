03c0bc99eb05fe87957c58fbebc189971d2977d4
huangyuzai <1792132332@qq.com>
2018-12-27 00:32:33 +0800

    优化左边菜单

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -4,7 +4,9 @@
     <div class="menu-wrapper">
       <ul class="menu-nav">
         <li class="menu-item" v-for="item of goods" :key="item.index">
-          {{item.name}}
+          <span class="text">
+            <span class="icon" :class="iconType[item.type]" v-show="item.type > 0"></span>{{item.name}}
+          </span>
         </li>
       </ul>
     </div>
@@ -36,11 +38,15 @@ export default {
         this.goods = data.data
       }
     })
+    // 图标
+    this.iconType = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+@import '../../common/stylus/mlxin.styl'
+
 .Goods
   display: flex
   position: absolute
@@ -52,6 +58,7 @@ export default {
     flex: 0 0 80px
     background: #f3f5f7
     box-sizing: border-box
+    overflow: hidden
     .menu-nav
       .menu-item
         font-size: 12px
@@ -59,10 +66,34 @@ export default {
         font-weight: 200
         line-height: 14px
         height: 54px
-        display: flex
+        display: table
         align-items: center
         border-bottom: solid 1px rgba(7,17,27,.1)
         padding: 0 12px
+        width: 100%
+        box-sizing: border-box
+        .text
+          display: table-cell
+          vertical-align: middle;
+          color: rgb(7,17,27);
+          .icon
+            display: inline-block
+            height: 12px
+            width: 12px
+            margin-right: 2px
+            background-color: #ddd
+            background-size: 12px
+            background-repeat: no-repeat
+          .decrease
+            bg-image('../../../static/image/decrease_3')
+          .discount
+            bg-image('../../../static/image/discount_3')
+          .special
+            bg-image('../../../static/image/special_3')
+          .invoice
+            bg-image('../../../static/image/invoice_3')
+          .guarantee
+            bg-image('../../../static/image/guarantee_3')
   .goods-wrapper
     flex: 1
 </style>
